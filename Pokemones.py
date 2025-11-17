a82a1997561208d77f3c3b46bbe0e5f05a234f3c
axeltrigoss <trigosaxel17@gmail.com>
2025-11-17 18:56:54 -0300

    Create Pokemones.py

---
+++ Pokemones.py
@@ -0,0 +1,98 @@
+# Lista de 30 Pokémones
+
+pokemons_30 = [
+    {"name":"Bulbasaur","number":1,"types":["Grass","Poison"],
+     "weaknesses":["Fire","Ice","Flying","Psychic"],"mega":False,"gigamax":False},
+
+    {"name":"Ivysaur","number":2,"types":["Grass","Poison"],
+     "weaknesses":["Fire","Ice","Flying","Psychic"],"mega":False,"gigamax":False},
+
+    {"name":"Venusaur","number":3,"types":["Grass","Poison"],
+     "weaknesses":["Fire","Ice","Flying","Psychic"],"mega":True,"gigamax":True},
+
+    {"name":"Charmander","number":4,"types":["Fire"],
+     "weaknesses":["Water","Ground","Rock"],"mega":False,"gigamax":False},
+
+    {"name":"Charmeleon","number":5,"types":["Fire"],
+     "weaknesses":["Water","Ground","Rock"],"mega":False,"gigamax":False},
+
+    {"name":"Charizard","number":6,"types":["Fire","Flying"],
+     "weaknesses":["Water","Electric","Rock"],"mega":True,"gigamax":True},
+
+    {"name":"Squirtle","number":7,"types":["Water"],
+     "weaknesses":["Grass","Electric"],"mega":False,"gigamax":False},
+
+    {"name":"Wartortle","number":8,"types":["Water"],
+     "weaknesses":["Grass","Electric"],"mega":False,"gigamax":False},
+
+    {"name":"Blastoise","number":9,"types":["Water"],
+     "weaknesses":["Grass","Electric"],"mega":True,"gigamax":True},
+
+    {"name":"Pikachu","number":25,"types":["Electric"],
+     "weaknesses":["Ground"],"mega":False,"gigamax":True},
+
+    {"name":"Raichu","number":26,"types":["Electric"],
+     "weaknesses":["Ground"],"mega":False,"gigamax":False},
+
+    {"name":"Jigglypuff","number":39,"types":["Normal","Fairy"],
+     "weaknesses":["Steel","Poison"],"mega":False,"gigamax":False},
+
+    {"name":"Wigglytuff","number":40,"types":["Normal","Fairy"],
+     "weaknesses":["Steel","Poison"],"mega":False,"gigamax":False},
+
+    {"name":"Zubat","number":41,"types":["Poison","Flying"],
+     "weaknesses":["Electric","Ice","Psychic","Rock"],"mega":False,"gigamax":False},
+
+    {"name":"Golbat","number":42,"types":["Poison","Flying"],
+     "weaknesses":["Electric","Ice","Psychic","Rock"],"mega":False,"gigamax":False},
+
+    {"name":"Oddish","number":43,"types":["Grass","Poison"],
+     "weaknesses":["Fire","Ice","Flying","Psychic"],"mega":False,"gigamax":False},
+
+    {"name":"Gloom","number":44,"types":["Grass","Poison"],
+     "weaknesses":["Fire","Ice","Flying","Psychic"],"mega":False,"gigamax":False},
+
+    {"name":"Vileplume","number":45,"types":["Grass","Poison"],
+     "weaknesses":["Fire","Ice","Flying","Psychic"],"mega":False,"gigamax":False},
+
+    {"name":"Growlithe","number":58,"types":["Fire"],
+     "weaknesses":["Water","Ground","Rock"],"mega":False,"gigamax":False},
+
+    {"name":"Arcanine","number":59,"types":["Fire"],
+     "weaknesses":["Water","Ground","Rock"],"mega":False,"gigamax":False},
+
+    {"name":"Geodude","number":74,"types":["Rock","Ground"],
+     "weaknesses":["Water","Grass","Fighting","Ice","Ground","Steel"],
+     "mega":False,"gigamax":False},
+
+    {"name":"Graveler","number":75,"types":["Rock","Ground"],
+     "weaknesses":["Water","Grass","Fighting","Ice","Ground","Steel"],
+     "mega":False,"gigamax":False},
+
+    {"name":"Golem","number":76,"types":["Rock","Ground"],
+     "weaknesses":["Water","Grass","Fighting","Ice","Ground","Steel"],
+     "mega":False,"gigamax":False},
+
+    {"name":"Magnemite","number":81,"types":["Electric","Steel"],
+     "weaknesses":["Fire","Fighting","Ground"],"mega":False,"gigamax":False},
+
+    {"name":"Magneton","number":82,"types":["Electric","Steel"],
+     "weaknesses":["Fire","Fighting","Ground"],"mega":False,"gigamax":False},
+
+    {"name":"Gengar","number":94,"types":["Ghost","Poison"],
+     "weaknesses":["Ghost","Psychic","Dark","Ground"],
+     "mega":True,"gigamax":True},
+
+    {"name":"Onix","number":95,"types":["Rock","Ground"],
+     "weaknesses":["Water","Grass","Fighting","Ice","Ground","Steel"],
+     "mega":False,"gigamax":False},
+
+    {"name":"Voltorb","number":100,"types":["Electric"],
+     "weaknesses":["Ground"],"mega":False,"gigamax":False},
+
+    {"name":"Electrode","number":101,"types":["Electric"],
+     "weaknesses":["Ground"],"mega":False,"gigamax":False},
+
+    {"name":"Jolteon","number":135,"types":["Electric"],
+     "weaknesses":["Ground"],"mega":False,"gigamax":False},
+]
